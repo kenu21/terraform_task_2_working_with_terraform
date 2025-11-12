2af5b7fd35ec50ec7e585b037291ec26923ea2a0
Yurii <yurij.kenu@gmail.com>
2025-11-12 10:11:01 +0100

    storage: added excludes

--- archive.tf
+++ archive.tf
@@ -2,4 +2,7 @@ data "archive_file" "terraform_task_2_zip" {
   type        = "zip"
   source_dir  = path.module
   output_path = "${path.module}/terraform_task_2.zip"
+  excludes = [
+    "${path.module}/terraform_task_2.zip"
+  ]
 }
